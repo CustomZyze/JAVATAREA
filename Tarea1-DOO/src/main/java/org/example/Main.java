5f9735d2f25d2a338f04ff8d5b35696b167be951
Matias Cabello <maticab170@gmail.com>
2026-05-01 20:56:23 -0400

    test: Agregar prueba inicial de compra en Main

--- Tarea1-DOO/src/main/java/org/example/Main.java
+++ Tarea1-DOO/src/main/java/org/example/Main.java
@@ -1,10 +1,16 @@
 package org.example;
-
+import java.util.ArrayList;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main(String[] args) {
-
-
+    public static void main(String[] args) {
+        Expendedor exp = new Expendedor(5,4);
+        Moneda m1000 = new Moneda1000();
+        try {
+            Comprador c1 = new Comprador(m1000, Enumeracion.COCA_COLA, exp);
+            System.out.println("Compraste: " + c1.queConsumiste());
+        } catch (Exception e) {
+            System.out.println("Error en la compra: " + e.getMessage());
+        }
     }
 }
