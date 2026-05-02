862f77e7956373822a89808f00c24e523c7d2912
Matias Cabello <maticab170@gmail.com>
2026-05-02 02:06:54 -0400

    feat: Implementacion completa del main de prueba

--- Tarea1-DOO/src/main/java/org/example/Main.java
+++ Tarea1-DOO/src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("TESTEO DE EXCEPCIONES");
         //Prueba NoHayStock
         try {
             Moneda m1000 = new Moneda1000();
@@ -49,21 +50,42 @@ public class Main {
             System.out.println("Error en la compra: Dinero insuficiente.");
         }
 
+        System.out.println();
+
         //Prueba Normal
-        try {
-            Moneda m1000 = new Moneda1000();
-            Expendedor exp1 = new Expendedor(12);
-            Comprador c1 = new Comprador(m1000, Enumeracion.SNICKERS, exp1);
-            System.out.println("Compraste: " + c1.queConsumiste());
-            System.out.println("Vuelto: " + c1.cuantoVuelto());
-        } catch (NoHayProductoException e) {
-            System.out.println("Error en la compra: No queda stock.");
-        } catch (PagoIncorrectoException e) {
-            System.out.println("Error en la compra: Moneda nula.");
-        } catch (PagoInsuficienteException e) {
-            System.out.println("Error en la compra: Dinero insuficiente.");
+        System.out.println("PRUEBA DE CADA PRODUCTO");
+        Moneda m1000 = new Moneda1000();
+        Expendedor exp1 = new Expendedor(12);
+        for (Enumeracion prod : Enumeracion.values()) {
+            try {
+                Comprador c1 = new Comprador(m1000, prod, exp1);
+                System.out.println("Compraste: " + c1.queConsumiste());
+                System.out.println("Vuelto: " + c1.cuantoVuelto());
+            } catch (NoHayProductoException e) {
+                System.out.println("Error en la compra: No queda stock.");
+            } catch (PagoIncorrectoException e) {
+                System.out.println("Error en la compra: Moneda nula.");
+            } catch (PagoInsuficienteException e) {
+                System.out.println("Error en la compra: Dinero insuficiente.");
+            }
         }
 
+        //Prueba sorteo moneda
+        ArrayList<Moneda> listaMonedas = new ArrayList<>();
+
+        listaMonedas.add(new Moneda100());
+        listaMonedas.add(new Moneda1000());
+        listaMonedas.add(new Moneda500());
+        listaMonedas.add(new Moneda100());
+        listaMonedas.add(new Moneda100());
+        listaMonedas.add(new Moneda500());
+        listaMonedas.add(new Moneda1000());
+        listaMonedas.add(new Moneda100());
+        listaMonedas.add(new Moneda500());
+
+        System.out.println("Lista desordenada: " + listaMonedas);
+        listaMonedas.sort(null);
+        System.out.println("Lista ordenada: " + listaMonedas);
 
     }
 }
