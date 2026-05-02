af7dc59f9a74aff56785251980490a9ab847a312
Matias Cabello <maticab170@gmail.com>
2026-05-02 00:34:36 -0400

    test: implementar casos de prueba para excepciones y caso normal en Main

--- Tarea1-DOO/src/main/java/org/example/Main.java
+++ Tarea1-DOO/src/main/java/org/example/Main.java
@@ -3,13 +3,67 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Expendedor exp = new Expendedor(5);
-        Moneda m1000 = new Moneda1000();
+
+        //Prueba NoHayStock
         try {
-            Comprador c1 = new Comprador(m1000, Enumeracion.COCA_COLA, exp);
+            Moneda m1000 = new Moneda1000();
+            Expendedor exp1 = new Expendedor(0);
+            Comprador c1 = new Comprador(m1000, Enumeracion.SNICKERS, exp1);
             System.out.println("Compraste: " + c1.queConsumiste());
-        } catch (Exception e) {
-            System.out.println("Error en la compra: " + e.getMessage());
+            System.out.println("Vuelto: " + c1.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("Error en la compra: No queda stock.");
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Error en la compra: Moneda nula.");
+        } catch (PagoInsuficienteException e) {
+            System.out.println("Error en la compra: Dinero insuficiente.");
         }
+
+        //Prueba PagoIncorrecto
+        try {
+            Moneda m1000 = null;
+            Expendedor exp1 = new Expendedor(12);
+            Comprador c1 = new Comprador(m1000, Enumeracion.SNICKERS, exp1);
+            System.out.println("Compraste: " + c1.queConsumiste());
+            System.out.println("Vuelto: " + c1.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("Error en la compra: No queda stock.");
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Error en la compra: Moneda nula.");
+        } catch (PagoInsuficienteException e) {
+            System.out.println("Error en la compra: Dinero insuficiente.");
+        }
+
+        //Prueba PagoInsuficiente
+        try {
+            Moneda m100 = new Moneda100();
+            Expendedor exp1 = new Expendedor(12);
+            Comprador c1 = new Comprador(m100, Enumeracion.SNICKERS, exp1);
+            System.out.println("Compraste: " + c1.queConsumiste());
+            System.out.println("Vuelto: " + c1.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("Error en la compra: No queda stock.");
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Error en la compra: Moneda nula.");
+        } catch (PagoInsuficienteException e) {
+            System.out.println("Error en la compra: Dinero insuficiente.");
+        }
+
+        //Prueba Normal
+        try {
+            Moneda m1000 = new Moneda1000();
+            Expendedor exp1 = new Expendedor(12);
+            Comprador c1 = new Comprador(m1000, Enumeracion.SNICKERS, exp1);
+            System.out.println("Compraste: " + c1.queConsumiste());
+            System.out.println("Vuelto: " + c1.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println("Error en la compra: No queda stock.");
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Error en la compra: Moneda nula.");
+        } catch (PagoInsuficienteException e) {
+            System.out.println("Error en la compra: Dinero insuficiente.");
+        }
+
+
     }
 }
