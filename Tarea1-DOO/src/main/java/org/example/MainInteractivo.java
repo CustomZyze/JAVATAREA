0b71c67c01dada72ab5961694ccc6a960bc6d13c
Matias Cabello <maticab170@gmail.com>
2026-05-02 20:10:10 -0400

    feat: Terminar implementacion de Main Interactivo y condiciones de ciclo Scanner

--- Tarea1-DOO/src/main/java/org/example/MainInteractivo.java
+++ Tarea1-DOO/src/main/java/org/example/MainInteractivo.java
@@ -7,39 +7,57 @@ public class MainInteractivo {
         System.out.println("Maquina Expendedora");
         int eleccion = 0;
         boolean maquina = true;
-        Moneda1000 m1000 = new Moneda1000();
-        Expendedor exp1 = new Expendedor(10);
+        Expendedor exp1 = new Expendedor(13);
 
         while (maquina) {
-            System.out.println("Escoga que desea hacer:\n1.Comprar bebida\n2.Salir\n");
+            System.out.println("Escoga que desea hacer:\n1.Comprar producto\n2.Salir\n");
             eleccion = s.nextInt();
             switch (eleccion) {
                 case 1:
-                    System.out.println("BEBIDAS EN MAQUINA");
-                    System.out.println("\n1.Coca Cola\n2.Sprite\n3.Fanta");
-                    int bebida = s.nextInt();
-                    Enumeracion producto = null;
-                        if (bebida == 1) {
-                            producto = Enumeracion.COCA_COLA;
-                        }
-                        if (bebida == 2) {
-                            producto = Enumeracion.SPRITE;
-                        }
-                        if (bebida == 3) {
-                            producto = Enumeracion.FANTA;
-                        }
+                    System.out.println("PRODUCTOS EN MAQUINA");
+                    System.out.println("1.Coca Cola: $"+ Enumeracion.COCA_COLA.getPrecio()+"\n2.Sprite: $" +Enumeracion.SPRITE.getPrecio()+
+                            "\n3.Fanta: $"+Enumeracion.FANTA.getPrecio()+"\n4.Super8: $"+Enumeracion.SUPER8.getPrecio()+ "\n5.Snickers: $"
+                            +Enumeracion.SNICKERS.getPrecio() +"\n");
 
+                    int eleccion2 = s.nextInt();
+                    Moneda m = null;
+                    Enumeracion producto = null;
+                    switch (eleccion2){
+                        case 1 -> producto = Enumeracion.COCA_COLA;
+                        case 2 -> producto = Enumeracion.SPRITE;
+                        case 3 -> producto = Enumeracion.FANTA;
+                        case 4 -> producto = Enumeracion.SUPER8;
+                        case 5 -> producto = Enumeracion.SNICKERS;
+                    }
+
+
+                    System.out.println("PAGAR CON: ");
+                    System.out.println("1.$100\n2.$500\n3.1000");
+                    int moneda = s.nextInt();
+                    if(moneda == 1) {
+                        m = new Moneda100();
+                    }
+                    if(moneda == 2) {
+                        m = new Moneda500();
+                    }
+                    if(moneda == 3) {
+                        m = new Moneda1000();
+                    }
+
+                    Comprador c1;
                     try {
-                        Comprador c1 = new Comprador(m1000, producto, exp1);
-                        System.out.println("Compraste: " + c1.queConsumiste());
-                        System.out.println("Vuelto: " + c1.cuantoVuelto());
+                        c1 = new Comprador(m, producto, exp1);
+                        System.out.println("Consumiste: " + c1.queConsumiste());
+                        System.out.println("Vuelto: " + c1.cuantoVuelto()+"\n");
 
                     } catch (NoHayProductoException e) {
-                        System.out.println("Error en la compra: No queda stock.");
-                    } catch (PagoIncorrectoException e) {
-                        System.out.println("Error en la compra: Moneda nula.");
-                    } catch (PagoInsuficienteException e) {
-                        System.out.println("Error en la compra: Dinero insuficiente.");
+                        System.out.println("Error en la compra: " + e.getMessage());
+                        System.out.println("Vuelto: " + exp1.getVuelto());
+                    } catch (PagoIncorrectoException f) {
+                        System.out.println("Error en la compra: " + f.getMessage());
+                    } catch (PagoInsuficienteException g) {
+                        System.out.println("Error en la compra: " + g.getMessage());
+                        System.out.println("Vuelto: " + exp1.getVuelto());
                     }
                     break;
 
@@ -47,8 +65,9 @@ public class MainInteractivo {
                     System.out.println("GRACIAS");
                     maquina = false;
                     break;
+                default:
+                    System.out.println("Seleccion no valida, intente de nuevo.\n");
             }
-
         }
     }
 }
